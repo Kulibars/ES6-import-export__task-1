3771d71f99beba9cd71a4e3d2e897a83cadff5e0
Kulibars <125401672+Kulibars@users.noreply.github.com>
2023-12-24 11:06:36 +0300

    ES6 import export__task 1

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import { getRandomColor } from './utils.js'
+
+
+export default function initApp() {
+    const button = document.createElement('button');
+    button.className = 'button';
+    button.textContent = 'Изменить цвет страницы';
+    document.body.append(button);
+    button.addEventListener('click', () => {
+        document.body.style.backgroundColor = `${getRandomColor()}`;
+    })
+};
+
